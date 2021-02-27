506b16fd680dbd78ec09bcf8837659d8f7dd168d
Isabel-alt <60380073+Isabel-alt@users.noreply.github.com>
2021-02-26 17:20:25 -0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.cardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView questionTextView = findViewById(R.id.flashcard_question);
+        TextView answerTextView = findViewById(R.id.answer);
+
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
+
+
+
+            }
+        });
+
+    }
+}
